444e5de45b607efbeb31caa092ce43118f0b2967
Nila-AffinitiAI <nila.sivasankaran@affiniti.ai>
2022-05-11 12:28:15 +0200

    fix SQLITE_RANGE error

--- processor/db_operation.js
+++ processor/db_operation.js
@@ -105,13 +105,12 @@ class DBOperations {
         if (customer_id || transaction_hash || transaction_id) {
             const update_hash_query = `UPDATE transactions SET transaction_hash = ? WHERE customer_id = ? and transaction_id is null`;
             this.bank_operations.run(
-                update_hash_query, [transaction_hash, customer_id, transaction_id]
+                update_hash_query, [transaction_hash, customer_id]
             ).then(update_hash_res => {
                 console.log("update_hash_res", update_hash_res)
                 const remove_dup_query = `delete from transactions where rowid not in (select min(rowid) from transactions group by transaction_hash);`;
-                this.bank_operations.run(
-                    remove_dup_query
-                ).then(delete_dup_res => {
+                this.bank_operations.run(remove_dup_query)
+                .then(delete_dup_res => {
                     console.log("delete_dup_res", delete_dup_res)
                     const update_id_query = `UPDATE transactions SET transaction_id = ? WHERE customer_id = ? and transaction_hash = ?`;
                     return  this.bank_operations.run(
